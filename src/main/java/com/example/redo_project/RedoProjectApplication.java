59d175a20e4a2cb1ee335b5abf157f991c3dda79
longnguyencoder <long0398481719@gmail.com>
2025-03-18 20:37:11 +0700

    init project

---
+++ src/main/java/com/example/redo_project/RedoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.redo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redo_project/RedoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
